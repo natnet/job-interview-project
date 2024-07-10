2d24f1a90a866bef90fb27d0ee173086e2465947
natnet <natanbutbul@gmail.com>
2024-07-11 00:44:40 +0200

    config in preducer

--- producer/Event.py
+++ producer/Event.py
@@ -1,24 +1,29 @@
 import random
 import datetime
 import pytz
+import yaml
+import logging
 
 class Event:
-    reporter_id_counter = 1
-    israel_tz = pytz.timezone('Asia/Jerusalem')
-    
-    def __init__(self):
-        self.metric_id = random.randint(1, 10)
-        self.metric_value = random.randint(1, 100)
-        self.timestamp = datetime.datetime.now(Event.israel_tz).isoformat()
-        self.reporter_id = Event.reporter_id_counter
-        self.message = f"HELLO WORLD"
-        Event.reporter_id_counter += 1
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    counter = 0
+    def __init__(self, tz, reporter_id_start_at ,message ,id_jumps):
+        try:
+            self.metric_id = random.randint(1, 10)
+            self.metric_value = random.randint(1, 100)
+            self.timestamp = datetime.datetime.now(tz).isoformat()
+            self.reporter_id = Event.counter + reporter_id_start_at
+            self.message = message
+            Event.counter += id_jumps
+        except Exception as e:
+            Event.logger.error(f"Failed to cushilirabak event {e}")
 
     def to_dict(self):
-            return {
-                "reporter_id": self.reporter_id,
-                "timestamp": self.timestamp,
-                "message": self.message,
-                "metric_value": self.metric_value,
-                "metric_id": self.metric_id
-            }
+        return {
+            "reporter_id": self.reporter_id,
+            "timestamp": self.timestamp,
+            "message": self.message,
+            "metric_value": self.metric_value,
+            "metric_id": self.metric_id
+        }
--- producer/kafka_service.py
+++ producer/kafka_service.py
@@ -3,6 +3,7 @@ import os
 import time
 import six
 import sys
+import yaml
 import logging
 from kafka import KafkaProducer
 from Event import Event
@@ -13,18 +14,19 @@ if sys.version_info >= (3, 12, 0):
 class KafkaService:
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
-    
-    def __init__(self):
+   
+    def __init__(self,attempt_to_connect_to_kafka_interval):
+        self.attempt_to_connect_to_kafka_interval = attempt_to_connect_to_kafka_interval
         self.bootstrap_servers = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
         self.topic_name = os.getenv('TOPIC_NAME')
     
     def create_producer(self):
         try:
             producer_attempt = KafkaProducer(bootstrap_servers=self.bootstrap_servers)
-            self.logger.info("Successfully connected to Kafka.")
+            KafkaService.logger.info("Successfully connected to Kafka.")
             return producer_attempt
         except Exception as e:
-            self.logger.error(f"Error creating KafkaProducer: {e}")
+            KafkaService.logger.error(f"Error creating KafkaProducer: {e}")
             return None
     
     def try_to_create_producer(self):
@@ -32,13 +34,13 @@ class KafkaService:
         while self.producer is None:
             self.producer = self.create_producer()
             if self.producer is None:
-                self.logger.info("Retrying connection to Kafka...")
-                time.sleep(3)
+                KafkaService.logger.info("Retrying connection to Kafka...")
+                time.sleep(self.attempt_to_connect_to_kafka_interval)
                 
     def send_event_to_kafka(self, event:Event):
         try:
             event_dict = event.to_dict()
             self.producer.send(self.topic_name, json.dumps(event_dict).encode("utf-8"))
-            self.logger.info("Event sent to Kafka successfully.")
+            KafkaService.logger.info("Event sent to Kafka successfully.")
         except Exception as e:
-            self.logger.error(f"Error sending event to Kafka: {e}")
+            KafkaService.logger.error(f"Error sending event to Kafka: {e}")
